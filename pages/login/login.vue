8c3c9180ca6b5997061c48e7918105ce609f3a8c
Mrzhzhzh <40493350+Mrzhzhzh@users.noreply.github.com>
2020-04-10 19:08:33 +0800

    test

--- pages/login/login.vue
+++ pages/login/login.vue
@@ -9,9 +9,10 @@
 				</view>
 				<view class="item flex borderB1 flexRowBetween">
 					<view style="width: 50%;">
-						<input type="text"  value="" placeholder="验证码">
+						<input type="text"  v-model="submitData.code" placeholder="验证码">
 					</view>
-					<view class="fs14 ftw color2">获取验证码</view>
+					<view class="fs14 ftw color2" @click="sendCode()" v-if="!hasSend">{{text}}</view>
+					<view class="fs14 ftw color2"  v-else>{{text}}</view>
 				</view>
 			</view>
 			<view class="pdt10 flex">
@@ -34,8 +35,12 @@
 			return {
 				Router:this.$Router,
 				submitData:{
-					login_name:''
-				}
+					login_name:'',
+					code:''
+				},
+				currentTime:61,
+				text:'获取验证码',
+				hasSend:false,
 			}
 		},
 		
@@ -46,10 +51,62 @@
 		},
 		methods: {
 			
+			sendCode(){
+				var self = this;
+				console.log(111)
+				if(self.hasSend){
+					return;
+				};
+				var phone = self.submitData.login_name;
+				
+				if (phone.trim().length != 11 || !/^1[3|4|5|6|7|8|9]\d{9}$/.test(phone)) {
+					self.$Utils.showToast('请输入正确的手机号', 'none', 1000)
+					
+					return;
+				}
+				var postData = {
+					data:{
+						phone:self.submitData.login_name,
+						type:2
+					}
+				};
+				var callback = function(res){
+					if(res.solely_code==100000){
+						uni.setStorageSync('canClick',true);
+						uni.hideLoading();
+						self.hasSend = true;
+						var interval = setInterval(function() {
+							self.currentTime--; //每执行一次让倒计时秒数减一
+						
+							self.text=self.currentTime + 's';//按钮文字变成倒计时对应秒数
+							
+							//如果当秒数小于等于0时 停止计时器 且按钮文字变成重新发送 且按钮变成可用状态 倒计时的秒数也要恢复成默认秒数 即让获取验证码的按钮恢复到初始化状态只改变按钮文字
+							if (self.currentTime <= 0) {
+								clearInterval(interval)
+								
+								self.hasSend = false;
+								self.text='重新发送';
+								self.currentTime= 61;
+								
+							}
+							
+						}, 1000);
+					}else{
+						uni.hideLoading();
+						self.$Utils.showToast('发送失败', 'none', 1000)
+					};
+				};
+				self.$apis.codeGet(postData, callback);
+			},
+			
 			submit() {
 				const self = this;
 				const postData = {
 					login_name: self.submitData.login_name,
+				};
+				postData.smsAuth = {
+					phone:self.submitData.login_name,						
+					code:self.submitData.code,
 				};
 				if (self.$Utils.checkComplete(self.submitData)) {
 					const callback = (res) => {
